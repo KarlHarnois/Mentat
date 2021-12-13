4de44c3170f4e484e6955dcfac9068d84492b255
KarlHarnois <karlrivestharnois@gmail.com>
2021-12-13 18:08:54 -0500

    Display transactions by day

--- Shared/Views/TransactionList.swift
+++ Shared/Views/TransactionList.swift
@@ -9,8 +9,25 @@ struct TransactionList: View {
     }
 
     var body: some View {
-        List(transactions) { transaction in
-            TransactionRow(transaction: transaction)
+        List {
+            ForEach(transactionDays) { day in
+                section(for: day)
+            }
         }
+    }
+
+    private func section(for transactionDay: TransactionDay) -> some View {
+        Section(header: Text(String(transactionDay.day))) {
+            ForEach(transactionDay.transactions, content: TransactionRow.init)
+        }
+    }
+
+    private var transactionDays: [TransactionDay] {
+        transactions
+            .sortedByDay()
+            .map { day, transactions in
+                TransactionDay(day: day, transactions: transactions)
+            }
+            .sorted(by: { $0.day < $1.day })
     }
 }
