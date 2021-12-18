9b831a4b2519f4041768a78fef9eb07f9f160585
KarlHarnois <karlrivestharnois@gmail.com>
2021-12-18 15:57:32 -0500

    Refactor summary builder

--- Shared/Utils/TransactionSummaryBuilder.swift
+++ Shared/Utils/TransactionSummaryBuilder.swift
@@ -1,66 +1,81 @@
 struct TransactionSummaryBuilder {
+    private typealias Sections = [Category: CategoryTransactionSummary]
+    private typealias Subsections = [Category: [Subcategory: SubcategoryTransactionSummary]]
+
     func summary(for transactions: [Transaction]) -> TransactionSummary {
         var summary = TransactionSummary(transactions: transactions)
-        var categorySections: [Category: CategoryTransactionSummary] = [:]
-        var subcategorySections: [Category: [Subcategory: SubcategoryTransactionSummary]] = [:]
+        var sections: Sections = [:]
+        var subSections: Subsections = [:]
 
         transactions.forEach { transaction in
-            summary.total += transaction.centAmount
+            let total = transaction.centAmount
+            summary.total += total
 
             if transaction.isPersonalExpense {
-                summary.expenseTotal += transaction.centAmount
+                summary.expenseTotal += total
             }
 
             guard let category = transaction.category else {
                 if transaction.isPersonalExpense {
-                    summary.uncategorizedExpenseTotal += transaction.centAmount
+                    summary.uncategorizedExpenseTotal += total
                 }
                 return
             }
 
-            if subcategorySections[category] == nil {
-                subcategorySections[category] = [:]
-            }
+            var section = findOrCreateSection(for: category, in: sections)
+            assign(transaction, to: &section)
+            sections[category] = section
 
-            var section = findOrCreateSection(for: category, in: categorySections)
-            section.total += transaction.centAmount
-            section.transactions.append(transaction)
-
-            if transaction.subcategory == nil {
-                section.uncategorizedTotal += transaction.centAmount
-            }
-
-            categorySections[category] = section
-
-            guard let subcategory = transaction.subcategory else {
-                return
-            }
-
-            var subsection = findOrCreateSubsection(for: category, and: subcategory, in: subcategorySections)
-            subsection.total += transaction.centAmount
-            subsection.transactions.append(transaction)
-            subcategorySections[category]?[subcategory] = subsection
+            guard let subcategory = transaction.subcategory else { return }
+            createSubsectionsIfNeeded(in: &subSections, for: category)
+            var subsection = findOrCreateSubsection(for: category, and: subcategory, in: subSections)
+            assign(transaction, to: &subsection)
+            subSections[category]?[subcategory] = subsection
         }
 
-        summary.categorySections = categorySections.map { category, section in
-            var copy = section
-            copy.subcategories = subcategorySections[category]?.map { _, value in value } ?? []
-            return copy
-        }
+        summary.categorySections = categorySummaries(from: sections, and: subSections)
         return summary
     }
 
-    private func findOrCreateSection(for category: Category, in dictionary: [Category: CategoryTransactionSummary]) -> CategoryTransactionSummary {
-        guard let section = dictionary[category] else {
+    private func findOrCreateSection(for category: Category, in sections: Sections) -> CategoryTransactionSummary {
+        guard let section = sections[category] else {
             return CategoryTransactionSummary(category: category)
         }
         return section
     }
 
-    private func findOrCreateSubsection(for category: Category, and subcategory: Subcategory, in dictionary: [Category: [Subcategory: SubcategoryTransactionSummary]]) -> SubcategoryTransactionSummary {
-        guard let section = dictionary[category]?[subcategory] else {
+    private func findOrCreateSubsection(for category: Category, and subcategory: Subcategory, in subsections: Subsections) -> SubcategoryTransactionSummary {
+        guard let section = subsections[category]?[subcategory] else {
             return SubcategoryTransactionSummary(subcategory: subcategory)
         }
         return section
+    }
+
+    private func createSubsectionsIfNeeded(in subsections: inout Subsections, for category: Category) {
+        if subsections[category] == nil {
+            subsections[category] = [:]
+        }
+    }
+
+    private func assign(_ transaction: Transaction, to summary: inout CategoryTransactionSummary) {
+        summary.total += transaction.centAmount
+        summary.transactions.append(transaction)
+
+        if transaction.subcategory == nil {
+            summary.uncategorizedTotal += transaction.centAmount
+        }
+    }
+
+    private func assign(_ transaction: Transaction, to summary: inout SubcategoryTransactionSummary) {
+        summary.total += transaction.centAmount
+        summary.transactions.append(transaction)
+    }
+
+    private func categorySummaries(from sections: Sections, and subsections: Subsections) -> [CategoryTransactionSummary] {
+        sections.map { category, section in
+            var copy = section
+            copy.subcategories = subsections[category]?.map { _, value in value } ?? []
+            return copy
+        }
     }
 }
